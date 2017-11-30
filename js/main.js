90823c96e87ffed142152e96d9d8e368eddaa4a5
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-11-30 13:31:52 -0600

    Adding proyect

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).on('load', function () {
+    var metas = $('meta');
+    var url = encodeURIComponent(window.location.href);
+    var description = '';
+    metas.each(function() {
+        if ($(this).attr('property') === 'og:description') {
+            description = $(this).attr('content');
+        }
+    });
+    $('#fbShare').attr('href', 'https://www.facebook.com/sharer/sharer.php?u='+url);
+    $('#tweetShare').attr('href', 'https://twitter.com/share?text=' + description + ' ' + url);
+});
